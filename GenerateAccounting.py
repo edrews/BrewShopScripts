c0c346e1ce7b2945c2a053f69aa2bcaf70fa21a2
Eric Drews <edrews@gmail.com>
2020-05-29 23:48:49 -0400

    Update GenerateAccounting.py

--- GenerateAccounting.py
+++ GenerateAccounting.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 '''
+1.3.0
+- Customize sales-by-item.csv to add:
+  - eCommerce quantity sold for each item
+  - Total quantity sold
+  - Append items if they don't already exist
+- Use input folders instead of single input CSV's
+- Tolerate not finding order items in stock file
+1.2.0
+- Searching stock entries now by both Name *and* SKU since neither is sufficient on its own
+1.1.1
+- Including quantity in COGS calculation
 1.1.0
 - Better error reporting for missing SKUs
 - Changed order name to email since it's always there
 - Added support for order delivery fee
 - Multiplying stock price by item quantity when comparing totals
-
 1.0.0
 - Initial Shopkeep accounting script
 '''
@@ -14,10 +24,12 @@ import csv
 import math
 import os
 
-STOCK_FILE = "stock.csv"
-ORDERS_FILE = "orders.csv"
-ITEM_SALES_FILE = "item_sales.csv"
-ORDER_SALES_FILE = "order_sales.csv"
+STOCK_FOLDER = "stock"
+ORDERS_FOLDER = "orders"
+SALES_BY_ITEM_FOLDER = "sales-by-item"
+ITEM_SALES_FILE = "out-item-sales.csv"
+ORDER_SALES_FILE = "out-order-sales.csv"
+TOTAL_SALES_FILE = "out-total-sales-by-item.csv"
 
 def getPath(fileName):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), fileName)
@@ -29,11 +41,35 @@ def readFromFile(fileName):
             for row in csv.DictReader(dataFile, skipinitialspace=True)]
     return data
 
+def readFromFolder(folderName):
+    allData = []
+    folderPath = getPath(folderName)
+    for fileName in os.listdir(folderPath):
+        filePath = os.path.join(folderPath, fileName)
+        with open(filePath, newline='') as dataFile:
+            data = [{k: str(v) for k, v in row.items()}
+                for row in csv.DictReader(dataFile, skipinitialspace=True)]
+        allData.extend(data)
+    return allData
+
 def getStockEntry(field, itemSku, itemName, stock):
     for stockItem in stock:
-        if stockItem['Store Code (SKU)'] == itemSku:
+        if stockItem['Name'].strip() == itemName.strip() or stockItem['Store Code (SKU)'] == itemSku:
             return stockItem[field]
-    raise Exception("Error: The SKU for \"" + itemName + "\" (" + itemSku + ") was not found in the " + STOCK_FILE + " file")
+    raise Exception("Neither \"" + itemName + "\" or its SKU \"" + itemSku + "\" was not found in the stock CSV files")
+
+def getOrderQuantityOfItem(itemName, orders):
+    itemCount = 0
+    for orderItem in orders:
+        if orderItem['name'].strip() == itemName.strip():
+            itemCount += toFloat(orderItem['quantity'])
+    return itemCount
+
+def getQuantityOfAllOrderItems(orders):
+    itemCount = 0
+    for orderItem in orders:
+        itemCount += toFloat(orderItem['quantity'])
+    return itemCount
 
 def toFloat(value):
     if (value == ''): value = 0
@@ -45,64 +81,162 @@ def printFloat(value):
 def printYesNo(value):
     return "Yes" if value else "NO!!!"
 
-def writeItemSales(orders, stock):
-    ITEM_FIELDS = ["Time", "Name", "Department", "Price (Stock)", "Price (eCommerce)", "Prices Equal?", "Tax Details", "Cost"]
+def writeItemSold(orderItem, stock, itemSalesWriter):
+    itemSku = orderItem['sku'].strip('"')
+    itemName = orderItem['name']
+    try:
+        stockPrice = toFloat(getStockEntry('Price', itemSku, itemName, stock))
+        itemCost = toFloat(getStockEntry('Cost', itemSku, itemName, stock))
+        department = getStockEntry('Department', itemSku, itemName, stock)
+    except Exception as e:
+        print(e)
+        stockPrice = 0
+        itemCost = 0
+        department = "NOT_AVAILABLE"
+    itemQuantity = toFloat(orderItem['quantity'])
+    reportedTotal = toFloat(orderItem['total'])
+    arePricesEqual = math.isclose(stockPrice * itemQuantity, reportedTotal, abs_tol=0.001)
+    itemSalesWriter.writerow({ \
+        'Time': orderItem['timestamp'], \
+        'Name': itemName, \
+        'Department': department, \
+        'Price (Stock)': printFloat(stockPrice), \
+        'Quantity': printFloat(itemQuantity), \
+        'Total Expected': printFloat(stockPrice * itemQuantity), \
+        'Total Reported (eCommerce)': printFloat(orderItem['total']), \
+        'Totals Equal?': printYesNo(arePricesEqual), \
+        'Tax Details': orderItem['tax_details'], \
+        'Item Cost': printFloat(itemCost), \
+        'Total Cost': printFloat(itemCost * itemQuantity), \
+    })
+
+def writeItemsSold(orders, stock):
+    ITEM_FIELDS = ["Time", "Name", "Department", "Price (Stock)", "Quantity", "Total Expected", "Total Reported (eCommerce)", "Totals Equal?", "Tax Details", "Item Cost", "Total Cost"]
     with open(getPath(ITEM_SALES_FILE), "w", newline='') as itemSalesFile:
         itemSalesWriter = csv.DictWriter(itemSalesFile, fieldnames = ITEM_FIELDS)
         itemSalesWriter.writeheader()
         for orderItem in orders:
-            itemSku = orderItem['sku'].strip('"')
-            itemName = orderItem['name']
-            stockPrice = toFloat(getStockEntry('Price', itemSku, itemName, stock))
-            itemQuantity = toFloat(orderItem['quantity'])
-            reportedTotal = toFloat(orderItem['total'])
-            arePricesEqual = math.isclose(stockPrice * itemQuantity, reportedTotal, abs_tol=0.001)
-            itemSalesWriter.writerow({ \
-                'Time': orderItem['timestamp'], \
-                'Name': itemName, \
-                'Department': getStockEntry('Department', itemSku, itemName, stock), \
-                'Price (Stock)': printFloat(stockPrice), \
-                'Price (eCommerce)': printFloat(orderItem['total']), \
-                'Prices Equal?': printYesNo(arePricesEqual), \
-                'Tax Details': orderItem['tax_details'], \
-                'Cost': printFloat(getStockEntry('Cost', itemSku, itemName, stock)), \
-            })
+            writeItemSold(orderItem, stock, itemSalesWriter)
 
-def writeOrderSales(orders, stock):
+def writeOrder(order, orderSalesWriter):
+    expectedTotal = toFloat(order['order_subtotal']) + toFloat(order['order_shipping']) - toFloat(order['discount']) + toFloat(order['order_tax'])
+    areTotalsEqual = math.isclose(expectedTotal, toFloat(order['order_total']), abs_tol=0.001)
+    orderSalesWriter.writerow({ \
+        'Order Number': order['order_number'], \
+        'Time': order['timestamp'], \
+        'Email': order['email'], \
+        'Subtotal': printFloat(order['order_subtotal']), \
+        'Delivery Fee': printFloat(order['order_shipping']), \
+        'Discount': printFloat("-"+order['discount']), \
+        'Tax': printFloat(order['order_tax']), \
+        'Expected Total': printFloat(expectedTotal), \
+        'Reported Total': printFloat(order['order_total']), \
+        'Totals Equal?': printYesNo(areTotalsEqual), \
+    })
+
+def writeOrders(orders, stock):
     ORDER_FIELDS = ["Order Number", "Time", "Email", "Subtotal", "Delivery Fee", "Discount", "Tax", "Expected Total", "Reported Total", "Totals Equal?"]
     with open(getPath(ORDER_SALES_FILE), "w", newline='') as orderSalesFile:
         orderSalesWriter = csv.DictWriter(orderSalesFile, fieldnames = ORDER_FIELDS)
         orderSalesWriter.writeheader()
         previousOrderNumber = -1
-        for orderItem in orders:
-            if orderItem['order_number'] == previousOrderNumber:
-                continue #Skip orders already accounted for
-            previousOrderNumber = orderItem['order_number']
-            expectedTotal = toFloat(orderItem['order_subtotal']) + toFloat(orderItem['order_shipping']) - toFloat(orderItem['discount']) + toFloat(orderItem['order_tax'])
-            areTotalsEqual = math.isclose(expectedTotal, toFloat(orderItem['order_total']), abs_tol=0.001)
-            orderSalesWriter.writerow({ \
-                'Order Number': orderItem['order_number'], \
-                'Time': orderItem['timestamp'], \
-                'Email': orderItem['email'], \
-                'Subtotal': printFloat(orderItem['order_subtotal']), \
-                'Delivery Fee': printFloat(orderItem['order_shipping']), \
-                'Discount': printFloat("-"+orderItem['discount']), \
-                'Tax': printFloat(orderItem['order_tax']), \
-                'Expected Total': printFloat(expectedTotal), \
-                'Reported Total': printFloat(orderItem['order_total']), \
-                'Totals Equal?': printYesNo(areTotalsEqual), \
-            })
+        for order in orders:
+            if order['order_number'] != previousOrderNumber: #Skip orders already accounted for
+                writeOrder(order, orderSalesWriter) 
+            previousOrderNumber = order['order_number']
 
-try:
-    print("Reading " + ORDERS_FILE + "...")
-    orders = readFromFile(ORDERS_FILE)
-    print("Reading " + STOCK_FILE + "...")
-    stock = readFromFile(STOCK_FILE)
-    print("Writing " + ORDER_SALES_FILE + "...")
-    writeOrderSales(orders, stock)
-    print("Writing " + ITEM_SALES_FILE + "...")
-    writeItemSales(orders, stock)
-    print("Files written successfully!")
-except Exception as e:
-    print(e)
-input("Press Enter to exit...")
+def getSaleData(sale):
+    quantity = toFloat(sale['Quantity Sold'])
+    return { \
+        'Name': sale['Item Description'], \
+        'Department': sale['Department'], \
+        'Category': sale['Category'], \
+        'Register Quantity Sold': quantity, \
+        'eCommerce Quantity Sold': 0, \
+        'Total Quantity Sold': quantity, \
+        'Quantity on Hand': sale['Quantity on Hand'], \
+        'Supplier': sale['Supplier'], \
+    }
+
+def getOrderData(order, stock):
+    quantity = toFloat(order['quantity'])
+    orderSku = order['sku'].strip()
+    orderName = order['name'].strip()
+    try:
+        department = getStockEntry('Department', orderSku, orderName, stock)
+        category = getStockEntry('Category', orderSku, orderName, stock)
+        onHand = getStockEntry('Quantity', orderSku, orderName, stock)
+        supplier = getStockEntry('Supplier', orderSku, orderName, stock)
+    except Exception as e:
+        print(e)
+        department = "NOT_AVAILABLE"
+        category = "NOT_AVAILABLE"
+        onHand = "NOT_AVAILABLE"
+        supplier = "NOT_AVAILABLE"
+    return { \
+        'Name': orderName, \
+        'Department': department, \
+        'Category': category, \
+        'Register Quantity Sold': 0, \
+        'eCommerce Quantity Sold': quantity, \
+        'Total Quantity Sold': quantity, \
+        'Quantity on Hand': onHand, \
+        'Supplier': supplier, \
+    }
+
+def addOrAppendOrder(order, stock, totalItemsSold):
+    for item in totalItemsSold:
+        if order['name'].strip() == item['Name'].strip():
+            quantity = toFloat(order['quantity'])
+            item['eCommerce Quantity Sold'] += quantity
+            item['Total Quantity Sold'] += quantity
+            return
+    data = getOrderData(order, stock)
+    totalItemsSold.append(data)
+
+def addOrAppendSale(sale, totalItemsSold):
+    for item in totalItemsSold:
+        if sale['Item Description'].strip() == item['Name'].strip():
+            quantity = toFloat(sale['Quantity Sold'])
+            item['Register Quantity Sold'] += quantity
+            item['Total Quantity Sold'] += quantity
+            return
+    data = getSaleData(sale)
+    totalItemsSold.append(data)
+
+def writeTotalItemsSold(sales, orders, stock):
+    totalItemsSold = []
+    for sale in sales:
+        addOrAppendSale(sale, totalItemsSold)
+    for order in orders:
+        addOrAppendOrder(order, stock, totalItemsSold)
+    FIELDS = ["Name", "Department", "Category", "Register Quantity Sold", "eCommerce Quantity Sold", "Total Quantity Sold", "Quantity on Hand", "Supplier"]
+    with open(getPath(TOTAL_SALES_FILE), "w", newline='') as totalSalesFile:
+        totalSalesWriter = csv.DictWriter(totalSalesFile, fieldnames = FIELDS)
+        totalSalesWriter.writeheader()
+        for item in totalItemsSold:
+            totalSalesWriter.writerow(item)
+
+def generateAccounting():
+    try:
+        print("Reading " + ORDERS_FOLDER + " folder...")
+        orders = readFromFolder(ORDERS_FOLDER)
+        print("Reading " + STOCK_FOLDER + " folder...")
+        stock = readFromFolder(STOCK_FOLDER)
+        print("Writing " + ORDER_SALES_FILE + "...")
+        writeOrders(orders, stock)
+        print("Writing " + ITEM_SALES_FILE + "...")
+        writeItemsSold(orders, stock)
+        if os.path.exists(getPath(SALES_BY_ITEM_FOLDER)):
+            print("Reading " + SALES_BY_ITEM_FOLDER + " folder...")
+            sales = readFromFolder(SALES_BY_ITEM_FOLDER)
+            print("Writing " + TOTAL_SALES_FILE + "...")
+            writeTotalItemsSold(sales, orders, stock)
+        else:
+            print("Skipping " + TOTAL_SALES_FILE + "...")
+        print("All files written successfully!")
+    except Exception as e:
+        print("Error: " + e)
+    input("Press Enter to exit...")
+
+generateAccounting()
